31a03d351a9f058642b5372221778ba07925c996
StaceyCreates <stacey.onuora@mypatricia.co>
2024-05-26 14:07:08 +0100

    fix error

--- server.js
+++ server.js
@@ -31,93 +31,93 @@ const server = http.createServer((req, res) => {
             res.end(data);
         });
     } else if (method === 'POST' && url === '/submit') {
-        // let body = '';
+        let body = '';
 
-        // req.on('data', (chunk) => {
-        //     body += chunk.toString();
-        // });
-
-        // req.on('end', () => {
-        //     const formData = JSON.parse(body);
-        //     let jsonData = [];
-
-        //     let filePath = '/tmp/database.json';
-
-        //     fs.readFile(filePath, (err, data) => {
-        //         if (err) {
-        //             console.error('Error reading file:', err);
-        //             res.writeHead(500);
-        //             res.end(JSON.stringify({ message: 'Internal Server Error' }));
-        //             return;
-        //         }
-
-        //         if (data.length > 0) {
-        //             jsonData = JSON.parse(data);
-        //         }
-
-        //         jsonData.push(formData);
-
-        //         jsonData = JSON.stringify(jsonData, null, 2);
-
-        //         fs.writeFile(filePath, jsonData, (err) => {
-        //             if (err) {
-        //                 console.error('Error writing to file:', err);
-        //                 res.writeHead(500);
-        //                 res.end(JSON.stringify({ message: 'Internal Server Error' }));
-        //                 return;
-        //             }
-        //             console.log('Form data saved successfully.');
-        //             console.log('File content:', jsonData);
-        //             res.writeHead(200);
-        //             res.end(JSON.stringify({ message: 'Form data saved successfully.' }));
-        //         });
-        //     });
-        // });
-
-
-        let body = [];
-
-        req.on("data", (chunk) => {
-            body.push(chunk);
+        req.on('data', (chunk) => {
+            body += chunk.toString();
         });
 
-        req.on("end", () => {
-            let goo = JSON.parse(Buffer.concat(body).toString());
-            let db;
+        req.on('end', () => {
+            const formData = JSON.parse(body);
+            let jsonData = [];
 
-            fs.readFile(
-                path.join(__dirname, "database.json"),
-                "utf-8",
-                (err, data) => {
-                    if (err) console.log(err);
-                    db = JSON.parse(data);
-                    db.push(goo);
+            let filePath = path.join(__dirname, "database.json");
 
-                    fs.writeFile(
-                        "./database.json",
-                        JSON.stringify(db, null, 2),
-                        (err) => {
-                            if (err) console.log(err);
-                        }
-                    );
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    console.error('Error reading file:', err);
+                    res.writeHead(500);
+                    res.end(JSON.stringify({ message: 'Internal Server Error' }));
+                    return;
                 }
-            );
 
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "OPTIONS, POST",
-                "Access-Control-Allow-Headers": "content-type",
+                if (data.length > 0) {
+                    jsonData = JSON.parse(data);
+                }
+
+                jsonData.push(formData);
+
+                jsonData = JSON.stringify(jsonData, null, 2);
+
+                fs.writeFile(filePath, jsonData, (err) => {
+                    if (err) {
+                        console.error('Error writing to file:', err);
+                        res.writeHead(500);
+                        res.end(JSON.stringify({ message: 'Internal Server Error' }));
+                        return;
+                    }
+                    console.log('Form data saved successfully.');
+                    console.log('File content:', jsonData);
+                    res.writeHead(200);
+                    res.end(JSON.stringify({ message: 'Form data saved successfully.' }));
+                });
             });
-
-            res.write(
-                JSON.stringify({
-                    status: "success",
-                    msg: goo,
-                })
-            );
-            res.end();
         });
+
+
+        // let body = [];
+
+        // req.on("data", (chunk) => {
+        //     body.push(chunk);
+        // });
+
+        // req.on("end", () => {
+        //     let goo = JSON.parse(Buffer.concat(body).toString());
+        //     let db;
+
+        //     fs.readFile(
+        //         path.join(__dirname, "database.json"),
+        //         "utf-8",
+        //         (err, data) => {
+        //             if (err) console.log(err);
+        //             db = JSON.parse(data);
+        //             db.push(goo);
+
+        //             fs.writeFile(
+        //                 "./database.json",
+        //                 JSON.stringify(db, null, 2),
+        //                 (err) => {
+        //                     if (err) console.log(err);
+        //                 }
+        //             );
+        //         }
+        //     );
+
+        //     res.writeHead(200, {
+        //         "Content-Type": "application/json",
+        //         "Access-Control-Allow-Origin": "*",
+        //         "Access-Control-Allow-Methods": "OPTIONS, POST",
+        //         "Access-Control-Allow-Headers": "content-type",
+        //     });
+
+        //     res.write(
+        //         JSON.stringify({
+        //             status: "success",
+        //             msg: goo,
+        //         })
+        //     );
+        //     res.end();
+        // });
 
         return;
     } else {
