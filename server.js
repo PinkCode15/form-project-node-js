53cbc1186ebd761b1b991a3c78e0bf203a75d13e
StaceyCreates <stacey.onuora@mypatricia.co>
2024-05-26 11:36:10 +0100

    fix error

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const server = http.createServer((req, res) => {
             const formData = JSON.parse(body);
             let jsonData = [];
 
-            let filePath = path.join(__dirname, 'database.json');
+            let filePath = path.join(__dirname, "public", 'database.json');
 
             fs.readFile(filePath, (err, data) => {
                 if (err) {
